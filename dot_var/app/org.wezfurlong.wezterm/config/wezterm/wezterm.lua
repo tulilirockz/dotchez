8b57d34e5cfcdf0dfdc430226d78e978e05780b0
Tulip Blossom <tulilirockz@proton.me>
2025-04-03 12:46:08 -0300

    chore(auto): wezterm config

--- dot_var/app/org.wezfurlong.wezterm/config/wezterm/wezterm.lua
+++ dot_var/app/org.wezfurlong.wezterm/config/wezterm/wezterm.lua
@@ -3,7 +3,13 @@ local config = {
   color_scheme = 'Catppuccin Mocha',
   font = wezterm.font 'JetBrains Mono',
   font_size = 18,
-  default_prog = { "/home/linuxbrew/.linuxbrew/bin/zellij", "a", "-c", "mimiga" }
+  default_prog = { "/home/linuxbrew/.linuxbrew/bin/zellij", "a", "-c", "mimiga" },
+  enable_tab_bar = false,
+  window_close_confirmation = "NeverPrompt",
+  front_end = "WebGpu",
+  freetype_load_target = "Light",
+  use_resize_increments = true,
+  window_background_opacity = 0.8,
 }
 
 return config
